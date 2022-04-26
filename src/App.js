d724c767ccad4027e6a56dc76fd651a2befe55f7
Markus Johansson <markus.johansson@semcon.com>
2022-04-26 11:55:27 +0200

    configured for Demo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import config from './config/';
 import './App.css';
 
 function App() {
@@ -7,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          mode: {config.mode}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
